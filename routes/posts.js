e4c5d38819c604da14d1f30d2f8f53e10e915c2e
Kevin W <kwen1000@users.noreply.github.com>
2021-07-30 18:57:07 -0700

    email and password reset to email model

--- routes/posts.js
+++ routes/posts.js
@@ -1,11 +1,14 @@
 var express = require('express');
 var router = express.Router();
 
-const { User } = require('../models');
+// const { Post, User } = require('../models');
+
+const Post = require('../models').Post;
+const User = require('../models').User;
 
 router.get('/', (req, res, next) => {
-  User.findAll({ 
-    attributes: { exclude: ['password'] }
+  Post.findAll({
+    // include: [{ model: User, as: 'user' }]
   }).then(data => {
     res.status(200).send(data);
   }).catch(err => {
